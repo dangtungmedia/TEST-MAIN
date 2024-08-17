dccfb49e7f582628393a2ca6a79334f04901baef
Dang tung <dangtungmedia@gmail.com>
2024-08-17 11:39:09 +0700

    update  lại

--- core/settings.py
+++ core/settings.py
@@ -32,7 +32,7 @@ ASSETS_ROOT = os.getenv('ASSETS_ROOT', '/static/assets')
 
 # load production server from .env
 ALLOWED_HOSTS        = ['localhost', '127.0.0.1',env('SERVER', default='127.0.0.1'),'celery','daphne' ]
-CSRF_TRUSTED_ORIGINS = ['http://localhost','http://192.168.1.28:5085','http://localhost:5085','http://127.0.0.1','http://' + env('SERVER', default='127.0.0.1') + ':5085' ]
+CSRF_TRUSTED_ORIGINS = ['http://localhost','http://127.0.0.1:5085','http://localhost:5085','http://127.0.0.1','http://' + env('SERVER', default='127.0.0.1') + ':5085' ]
 # Application definition
 
 INSTALLED_APPS = [
