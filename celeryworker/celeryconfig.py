571ad7a7bfef41c5ebb70d9039b02dbf39c3d653
dangtung <dangtungmedia@gmail.com>
2024-10-29 16:58:06 +0700

    update lai time

--- celeryworker/celeryconfig.py
+++ celeryworker/celeryconfig.py
@@ -10,7 +10,8 @@ result_backend  = os.environ.get('CELERY_RESULT_BACKEND', 'redis://127.0.0.1:637
 accept_content  = ['json']
 task_serializer  = 'json'
 result_serializer  = 'json'
-timezone  = 'UTC'
+timezone  = 'Asia/Ho_Chi_Minh'
+enable_utc = True
 worker_timeout = 120
 worker_heartbeat_interval = 60
 
