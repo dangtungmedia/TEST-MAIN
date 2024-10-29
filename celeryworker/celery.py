37eb7c3e7cd0ae82a13d68cb1acd9c72dd121087
dangtung <dangtungmedia@gmail.com>
2024-10-29 17:48:31 +0700

    updat lai lan2

--- celeryworker/celery.py
+++ celeryworker/celery.py
@@ -1,6 +1,4 @@
 from celery import Celery
-from celery import Celery
-
 
 # Tạo ứng dụng Celery với tên 'celeryworker'
 app = Celery('celeryworker')
@@ -9,15 +7,15 @@ app = Celery('celeryworker')
 app.config_from_object('celeryworker.celeryconfig')
 
 # Tự động tìm kiếm và nạp các task từ các module chỉ định trong package 'celeryworker'
-app.autodiscover_tasks(['celeryworker'])  # Chỉ định đúng tên package
+app.autodiscover_tasks(['celeryworker'])
 
 # Thiết lập timezone cho Celery
-app.conf.timezone = 'UTC'
-app.conf.timezone = 'asia/ho_chi_minh'
+app.conf.timezone = 'Asia/Ho_Chi_Minh'  # Sử dụng đúng cú pháp với chữ hoa
+
+# Các cấu hình khác
 app.conf.worker_timeout = 120
 app.conf.worker_heartbeat_interval = 30
 app.conf.broker_connection_retry_on_startup = True
-
 app.conf.accept_content = ['json']
 app.conf.task_serializer = 'json'
 app.conf.result_serializer = 'json'
