db9bed6e1ccf530c1bd7f1e0e333d347543f396f
dangtung <dangtungmedia@gmail.com>
2024-10-30 14:37:45 +0700

    update downd load screen

--- Dockerfile
+++ Dockerfile
@@ -18,3 +18,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 WORKDIR /app
 
+RUN download.sh
---
+++ download.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ ! -d /app/video_screen ]; then
+    echo 'Thư mục chưa tồn tại, tiến hành tải và giải nén.'
+    wget -O /app/video_screen.rar 'http://157.90.208.177:8000/download_file_screen/' &&
+    mkdir -p /app/video_screen &&
+    unrar x /app/video_screen.rar /app/video_screen
+
+    if [ $? -eq 0 ]; then
+        rm -f /app/video_screen.rar
+        echo 'done' > /app/video_screen/ready.txt
+    else
+        echo 'Giải nén thất bại. Xóa file nén.'
+        rm -f /app/video_screen.rar
+    fi
+else
+    echo 'Thư mục đã tồn tại, không tải và giải nén lại.'
+    echo 'done' > /app/video_screen/ready.txt
+fi
+
+# Giữ cho container không bị thoát
+tail -f /dev/null
