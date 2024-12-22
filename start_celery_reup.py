6b15aaa159c78f8f2b4984cab9a24fca2b3284fc
dangtung <dangtungmedia@gmail.com>
2024-12-22 10:58:41 +0700

    update code

--- start_celery_reup.py
+++ start_celery_reup.py
@@ -1,23 +1,38 @@
 import os
 import requests
+import socket
 
 def get_public_ip():
-    try:
-        # Sử dụng ipify API để lấy địa chỉ IPv4 public
-        response = requests.get("https://api.ipify.org")
-        if response.status_code == 200:
-            return response.text.strip()
-        else:
-            print(f"Failed to get public IP: {response.status_code}")
-            return None
-    except Exception as e:
-        print(f"Error getting public IP: {e}")
-        return None
+   try:
+       response = requests.get("https://api.ipify.org")
+       if response.status_code == 200:
+           return response.text.strip()
+       else:
+           print(f"Failed to get public IP: {response.status_code}")
+           return None
+   except Exception as e:
+       print(f"Error getting public IP: {e}")
+       return None
+
+def get_local_ip():
+   try:
+       # Lấy hostname
+       hostname = socket.gethostname()
+       # Lấy địa chỉ IP local
+       local_ip = socket.gethostbyname(hostname)
+       return local_ip
+   except Exception as e:
+       print(f"Error getting local IP: {e}")
+       return None
 
 if __name__ == "__main__":
-    # Lấy địa chỉ IP public
-    ip_address = get_public_ip()
-    # Chạy Celery worker với 2 worker và queue render_video
-    os.system(f"celery -A celeryworker worker -l INFO --hostname={ip_address}-Reup --concurrency=2 -Q render_video_reupload")
-    
-    
+   # Lấy địa chỉ IP public
+   public_ip = get_public_ip()
+   if public_ip == "27.72.143.24":
+       # Nếu IP public trùng khớp, sử dụng IP local
+       local_ip = get_local_ip()
+       if local_ip:
+           # Chạy Celery worker với IP local
+           os.system(f"celery -A celeryworker worker -l INFO --hostname={local_ip}-Reup --concurrency=2 -Q render_video_reupload")
+   else:
+       os.system(f"celery -A celeryworker worker -l INFO --hostname={public_ip}-Reup --concurrency=1 -Q render_video_reupload")
