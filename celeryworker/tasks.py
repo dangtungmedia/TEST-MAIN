84d28859ff055c369468e7d910f652f5d40c677f
dangtung <dangtungmedia@gmail.com>
2024-12-23 21:47:42 +0700

    update code

--- celeryworker/tasks.py
+++ celeryworker/tasks.py
@@ -2161,12 +2161,12 @@ def update_info_video(data, task_id, worker_id):
         total_size = int(response.headers.get('content-length', 0))
         downloaded_size = 0  # Kích thước đã tải
         with open(output_file, "wb") as file:
-            for chunk in response.iter_content(chunk_size=1024):  # Tải từng chunk
+            for chunk in response.iter_content(chunk_size=20024):  # Tải từng chunk
                 if chunk:  # Nếu chunk không rỗng
                     file.write(chunk)
                     downloaded_size += len(chunk)
                     percent_done = (downloaded_size / total_size) * 100
-                    update_status_video(f"Đang Render : Đã tải {percent_done:.2f}%", 
+                    update_status_video(f"Đang Render : Đã tải {percent_done:.2f}% video", 
                           video_id, task_id, worker_id)
         update_status_video("Đang Render : Đã tải xong video youtube", 
                           video_id, task_id, worker_id)
