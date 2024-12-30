fa477d40f411c185cba4f7d8dbe985ed7245c5dd
dangtung <dangtungmedia@gmail.com>
2024-12-30 17:22:46 +0700

    update code lan 1

--- celeryworker/celeryconfig.py
+++ celeryworker/celeryconfig.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 
 # Nạp biến môi trường từ file .env
 load_dotenv()
-
 broker_url  =  os.environ.get('CELERY_BROKER_URL', 'redis://127.0.0.1:6379/0' )
 result_backend  = os.environ.get('CELERY_RESULT_BACKEND', 'redis://127.0.0.1:6379/0')
 
